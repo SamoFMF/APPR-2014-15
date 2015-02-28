1b4431a1c0ed6acaf655994b09023dd8a55a7c17
Samo Metličar <samo.metlicar@hotmail.com>
2015-02-28 02:07:00 +0100

    Dodal še izpis tabel ter posodobil poročilo.

--- projekt.r
+++ projekt.r
@@ -12,5 +12,6 @@ source("vizualizacija/vizualizacija.r", encoding="UTF-8")
 # 4. faza: Napredna analiza podatkov
 source("analiza/animacija.r", encoding="UTF-8")
 source("analiza/analiza.r", encoding="UTF-8")
+source("lib/izpis_tabel.r", encoding="UTF-8")
 
 cat("Končano.\n")
